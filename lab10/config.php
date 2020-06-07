308c82a5c993239abe1a6664dc9f8485ac1c7496
Tieu-Lan <58111601+Tieu-Lan@users.noreply.github.com>
2020-06-07 21:19:02 +0800

    lab10

--- lab10/config.php
+++ lab10/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DBHOST', '');
-define('DBNAME', '');
-define('DBUSER', '');
-define('DBPASS', '');
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'Tieu');
+define('DBPASS', 'Tieu0106');
 define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
 ?>
